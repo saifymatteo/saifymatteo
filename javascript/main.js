6a45ca8790ecf5ea8bb9941f462c4de767d19b8f
saifymatteo <muhdsaifulmashuri@gmail.com>
2023-03-30 22:19:45 +0800

    Combines Portfolio repo to here

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+function myResume() {
+    window.open("https://drive.google.com/file/d/1Xco5x6n4Qy5P-WEzTdHt7v55dYjSl5Jo/view?usp=sharing");
+}
+
+function myWhatsApp() {
+    window.open("https://wa.link/xz2e7j");
+}
+
+function myEmail() {
+    window.open("mailto:muhdsaifulmashuri@gmail.com");
+}
