fa2ff9d9a7f873f75db0904a6e9a6efc9888f59c
Saiful Mashuri <dev@saifulmashuri.com>
2023-11-06 00:11:44 +0800

    [build] feat: add `build` folder for Netlify CI/CD

---
+++ build/javascript/main.js
@@ -0,0 +1,11 @@
+function myResume() {
+    window.open("https://github.com/saifymatteo/saifymatteo/releases/latest/download/Resume.Saiful.Mashuri.pdf");
+}
+
+function myWhatsApp() {
+    window.open("https://wa.link/xz2e7j");
+}
+
+function myEmail() {
+    window.open("mailto:work@saifulmashuri.com");
+}
