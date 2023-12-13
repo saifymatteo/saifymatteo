d65e38f1babbbcc30dc0585100307102c6d76433
Saiful Mashuri <dev@saifulmashuri.com>
2023-12-13 20:53:34 +0800

    [flutter_web] WIP: scroll to section from deep link

--- flutter_web/lib/src/pages/components/app_bar.dart
+++ flutter_web/lib/src/pages/components/app_bar.dart
@@ -1,85 +1,75 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:provider/provider.dart';
 
 import '../../../lib.dart';
 import 'components.dart';
 
 class MenuAppBar extends StatelessWidget {
-  const MenuAppBar({super.key});
+  const MenuAppBar({super.key, required this.onTapNavigation});
+
+  final ValueChanged<HomeSections> onTapNavigation;
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.appL10n;
     final maxConstraints = MediaQuery.sizeOf(context).width < 660;
 
-    return Consumer<AppState>(
-      builder: (context, state, __) {
-        return WebBodyBase(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          decoration: const BoxDecoration(color: AppTheme.black),
-          child: Stack(
-            children: [
-              AnimatedContainer(
-                duration: 300.milliseconds,
-                curve: Curves.fastEaseInToSlowEaseOut,
-                alignment:
-                    maxConstraints ? Alignment.centerLeft : Alignment.center,
-                child: InkWell(
-                  onTap: () => _onTapToScroll(
-                    appBarContext: context,
-                    keyContext: state.scrollContext[HomeSections.me],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: AppAssets.icons.logoCircle.image(),
-                  ),
-                ),
+    return WebBodyBase(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: const BoxDecoration(color: AppTheme.black),
+      child: Stack(
+        children: [
+          AnimatedContainer(
+            duration: 300.milliseconds,
+            curve: Curves.fastEaseInToSlowEaseOut,
+            alignment: maxConstraints ? Alignment.centerLeft : Alignment.center,
+            child: InkWell(
+              onTap: () => _onTapToScroll(
+                context: context,
+                section: HomeSections.me,
               ),
-              Positioned(
-                right: 0,
-                top: 0,
-                bottom: 0,
-                child: Row(
-                  children: [
-                    _ActionButton(
-                      labelText: l10n.portfolios,
-                      onPressed: () => _onTapToScroll(
-                        appBarContext: context,
-                        keyContext: state.scrollContext[HomeSections.portfolio],
-                      ),
-                    ),
-                    _ActionButton(
-                      labelText: l10n.contact,
-                      onPressed: () => _onTapToScroll(
-                        appBarContext: context,
-                        keyContext: state.scrollContext[HomeSections.contact],
-                      ),
-                    ),
-                  ],
-                ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: AppAssets.icons.logoCircle.image(),
               ),
-            ],
+            ),
           ),
-        );
-      },
+          Positioned(
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Row(
+              children: [
+                _ActionButton(
+                  labelText: l10n.portfolios,
+                  onPressed: () => _onTapToScroll(
+                    context: context,
+                    section: HomeSections.portfolio,
+                  ),
+                ),
+                _ActionButton(
+                  labelText: l10n.contact,
+                  onPressed: () => _onTapToScroll(
+                    context: context,
+                    section: HomeSections.contact,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   Future<void> _onTapToScroll({
-    required BuildContext appBarContext,
-    BuildContext? keyContext,
+    required BuildContext context,
+    required HomeSections section,
   }) async {
-    await appBarContext.router.navigate(const HomePageRoute());
+    await context.router.navigate(const HomePageRoute());
 
-    if (keyContext != null && appBarContext.mounted) {
-      Scrollable.ensureVisible(
-        keyContext,
-        duration: const Duration(milliseconds: 500),
-        curve: Curves.fastEaseInToSlowEaseOut,
-      );
-    }
+    onTapNavigation.call(section);
   }
 }
 
