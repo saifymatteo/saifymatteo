cf370bb724847e87d5a2ac412e5dc04f9fc3ae24
Saiful Mashuri <dev@saifulmashuri.com>
2023-12-13 20:25:48 +0800

    [flutter_web] fix: added `url_strategy` package

--- flutter_web/lib/main.dart
+++ flutter_web/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'src/app.dart';
 
 void main() async {
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
