58db53775a18eb1369965e06250bcc341eea6da0
Saiful Mashuri <github@saifulmashuri.com>
2026-07-30 12:21:08 +0800

    [flutter_web] chore: address lint

--- flutter_web/lib/pages/portfolio/page.dart
+++ flutter_web/lib/pages/portfolio/page.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
