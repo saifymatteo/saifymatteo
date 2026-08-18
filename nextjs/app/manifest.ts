443d20ab403cc45cd9119484a779edfe2d7abe76
Saiful Mashuri <dev@saifulmashuri.com>
2026-08-18 18:02:11 +0800

    [nextjs] feat: add favicon

---
+++ nextjs/app/manifest.ts
@@ -0,0 +1,23 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Saiful Mashuri',
+    short_name: 'Saiful',
+    icons: [
+      {
+        src: '/favicon/favicon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/favicon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    theme_color: '#0494df',
+    background_color: '#ffffff',
+    display: 'standalone',
+  };
+}
